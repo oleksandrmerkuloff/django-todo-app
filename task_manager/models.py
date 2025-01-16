86ed17a4fcbd917300a6648ea77c4f953a6650ea
Oleksandr Merkulov <oleksandrmerkuloff@gmail.com>
2025-01-16 22:54:09 +0200

    created model

--- task_manager/models.py
+++ task_manager/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    title = models.CharField(max_length=250)
+    created_date = models.DateTimeField(auto_now_add=True)
+    is_completed = models.BooleanField(default=True)
+
+    def __str__(self):
+        return str(self.title)
+
+    class Meta:
+        ordering = ['-created_date']
+        verbose_name = 'Task'
+        verbose_name_plural = 'Tasks'

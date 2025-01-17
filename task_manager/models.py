accdfd699132121b023d55a7b89c4a4b2d5b4acc
Oleksandr Merkulov <oleksandrmerkuloff@gmail.com>
2025-01-17 13:30:28 +0200

    migrate task models

--- task_manager/models.py
+++ task_manager/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Task(models.Model):
     title = models.CharField(max_length=250)
     created_date = models.DateTimeField(auto_now_add=True)
-    is_completed = models.BooleanField(default=True)
+    is_completed = models.BooleanField(default=False)
 
     def __str__(self):
         return str(self.title)
